d0976e5368f4ab173382330e53a0cfe82679bcd8
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-07-23 03:13:17 +0500

    programme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] =useState(0)
+
+  const subtraction = () =>{
+    setCount(count - 1)
+  }
+
+  const addition = () =>{
+    setCount(count + 1)
+  }
+
+  const reSet = () => {
+    setCount( 0)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+     <button onClick={subtraction} >-</button>
+      {count}
+      <button onClick={addition} >+</button>
+      <button onClick={reSet} >Reset</button>
     </div>
   );
 }
